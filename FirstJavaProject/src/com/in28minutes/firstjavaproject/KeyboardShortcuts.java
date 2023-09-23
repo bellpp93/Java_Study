80aaabe8a4160fe6997436faf3394aa0a937a496
JongIn Yoon <bellpp93@gmail.com>
2023-09-23 11:20:39 +0900

    Java_Study first commit

---
+++ FirstJavaProject/src/com/in28minutes/firstjavaproject/KeyboardShortcuts.java
@@ -0,0 +1,9 @@
+package com.in28minutes.firstjavaproject;
+
+public class KeyboardShortcuts {
+	public static void main(String[] args) {
+//		System.out.println("Short cut key practice");
+		int i = 0; // 지역 변수 초기화
+		System.out.println(i);
+	}
+}
