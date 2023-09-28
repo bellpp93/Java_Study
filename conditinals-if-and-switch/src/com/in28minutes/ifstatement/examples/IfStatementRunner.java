a5804ff7dc0de435996fe34f144abf3f91284bc9
JongIn Yoon <bellpp93@gmail.com>
2023-09-28 23:26:05 +0900

    230928_Study

---
+++ conditinals-if-and-switch/src/com/in28minutes/ifstatement/examples/IfStatementRunner.java
@@ -0,0 +1,14 @@
+package com.in28minutes.ifstatement.examples;
+
+public class IfStatementRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 25;
+		if (i == 25) {
+			System.out.println("i = 25");
+		} else {
+			System.out.println("i != 25");
+		}
+	}
+}
