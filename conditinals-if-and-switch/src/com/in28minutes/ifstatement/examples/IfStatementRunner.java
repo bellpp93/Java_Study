2bcd8d0705c4fd42f8e3a935230cdd2376753074
JongIn Yoon <bellpp93@gmail.com>
2023-09-29 00:05:29 +0900

    230929_if,else if,Scanner Study

--- conditinals-if-and-switch/src/com/in28minutes/ifstatement/examples/IfStatementRunner.java
+++ conditinals-if-and-switch/src/com/in28minutes/ifstatement/examples/IfStatementRunner.java
@@ -3,12 +3,20 @@ package com.in28minutes.ifstatement.examples;
 public class IfStatementRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int i = 25;
+		
+		int i = 24;
+		// i is 25
+		// i is 24
+		// i is neither 25 or 24
+		
 		if (i == 25) {
 			System.out.println("i = 25");
+		} else if (i == 24) {
+			System.out.println("i = 24");
+		} else if (i == 23) {
+			System.out.println("i = 23");
 		} else {
-			System.out.println("i != 25");
+			System.out.println("i != 25 and i != 24 and i != 23");
 		}
 	}
 }
