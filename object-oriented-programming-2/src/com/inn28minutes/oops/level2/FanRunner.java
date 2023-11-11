a8006000909bbb7243b72a8162ee7f0201d11d58
JongIn Yoon <bellpp93@gmail.com>
2023-11-11 12:22:14 +0900

    Java Study - State and Constructor

---
+++ object-oriented-programming-2/src/com/inn28minutes/oops/level2/FanRunner.java
@@ -0,0 +1,10 @@
+package com.inn28minutes.oops.level2;
+
+public class FanRunner {
+
+	public static void main(String[] args) {
+		Fan fan = new Fan("Manufacturer 1", 0.34567, "GREEN");
+		
+		System.out.println(fan);
+	}
+}
